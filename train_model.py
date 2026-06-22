eec4d408c0278f7b6e1698de95bbf3be2bf8b4fc
Swaroop Haridas <91984930+Swaroop-Haridas@users.noreply.github.com>
2026-06-22 11:30:41 +0530

    train_model.py

--- train_model.py
+++ train_model.py
@@ -7,7 +7,7 @@ data_dir = "train"
 model_path = "model.h5"
 label_map_path = "labels.npy"
 
-emotions = ["angry", "disgust", "fearful", "happy", "neutral", "sad", "surprise"]
+emotions = ["angry", "disgust", "fear", "happy", "neutral", "sad", "surprise"]
 
 faces = []
 labels = []
